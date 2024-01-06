2c79112634bd7a343cf5555f1f652d78d6787312
deandrebaker <deandreb54321@gmail.com>
2024-01-05 21:29:20 -0500

    Queues

--- src/main/java/Queues/Queue.java
+++ src/main/java/Queues/Queue.java
@@ -3,39 +3,39 @@ package Queues;
 import LinkedLists.LinkedList;
 
 public class Queue<T> {
-    private final LinkedList<T> linkedList;
+    private final LinkedList<T> items;
 
     public Queue() {
-        linkedList = new LinkedList<>();
+        items = new LinkedList<>();
     }
 
     public Queue(Iterable<T> initialItems) {
-        linkedList = new LinkedList<>(initialItems);
+        items = new LinkedList<>(initialItems);
     }
 
     public T peak() {
-        return linkedList.getHead();
+        return items.getHead();
     }
 
     public void enqueue(T item) {
-        linkedList.append(item);
+        items.append(item);
     }
 
     public T dequeue() {
-        T item = linkedList.getHead();
-        linkedList.deleteHead();
+        T item = items.getHead();
+        items.deleteHead();
         return item;
     }
 
     public int getSize() {
-        return linkedList.getSize();
+        return items.getSize();
     }
 
     public boolean isEmpty() {
-        return linkedList.isEmpty();
+        return items.isEmpty();
     }
 
     public String toString() {
-        return linkedList.toString();
+        return items.toString();
     }
 }
